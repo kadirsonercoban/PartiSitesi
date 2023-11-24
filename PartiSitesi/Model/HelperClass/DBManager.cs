25367c916e576f1c260d3561374623f6ef14e703
Kadir Soner Çoban <kadirsonercoban@gmail.com>
2023-11-24 12:52:52 +0300

    Proje dosyası ekle.

---
+++ PartiSitesi/Model/HelperClass/DBManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace PartiSitesi.Model.HelperClass
+{
+    public class DBManager
+    {
+        private static readonly string connString  = "Host=localhost;Username=postgres;Password=12345;Database=political_party_db";
+        public static NpgsqlConnection GetConnection()
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(connString);
+            return connection;
+        }
+    }
+}
