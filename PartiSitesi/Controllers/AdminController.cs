caa41973fa37a15b646ef192c8bc30c415eaf1a4
Kadir Soner Çoban <kadirsonercoban@gmail.com>
2023-11-27 17:08:41 +0300

    controller ve viewlar eklendi

---
+++ PartiSitesi/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartiSitesi.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminPage()
+        {
+            return View();
+        }
+
+        public IActionResult AddNews()
+        {
+            return View();
+        }
+    }
+}
